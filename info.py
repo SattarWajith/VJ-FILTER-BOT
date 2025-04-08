51bad7bb1c42c9aa8ef1ad74b1bfa952a397d722
SattarWajith <96818272+SattarWajith@users.noreply.github.com>
2025-04-09 02:11:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -115,7 +115,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 VERIFY = bool(environ.get('VERIFY', True))
 VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'short2url.in')
 VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '172a8b05bcc3287a4a1d7ee5b840b5d1bf409839')
-VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', '')
+VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/flshtml/68')
 
 # If You Fill Second Shortner Then Bot Attach Both First And Second Shortner And Use It For Verify.
 VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', True))
