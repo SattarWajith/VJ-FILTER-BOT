8b8179d8cd4b3f2a23f07cd6f6e39b28e71c0959
SattarWajith <96818272+SattarWajith@users.noreply.github.com>
2025-04-09 03:30:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -128,7 +128,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'short2url.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', '172a8b05bcc3287a4a1d7ee5b840b5d1bf409839')
-TUTORIAL = environ.get('TUTORIAL', '') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/flshtml/112') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
 # Others
